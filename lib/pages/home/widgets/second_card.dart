1599ab8d3e64a777ecebeefb3232ec80baadb93b
GabrielEagle <dialixkat@gmail.com>
2023-02-24 22:28:19 -0300

    third card done, app complete

--- lib/pages/home/widgets/second_card.dart
+++ lib/pages/home/widgets/second_card.dart
@@ -1,17 +1,142 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
 import 'package:flutter/material.dart';
 
-class SecondCard extends StatelessWidget {
+class SecondCard extends StatefulWidget {
   const SecondCard({super.key});
 
   @override
+  State<SecondCard> createState() => _SecondCardState();
+}
+
+class _SecondCardState extends State<SecondCard> with AutomaticKeepAliveClientMixin {
+  bool _showMoney = false;
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(),
-        Container(
-          color: Colors.grey,
-        )
-      ],
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5),
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Icon(
+                              Icons.attach_money,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(
+                              width: 5,
+                            ),
+                            Text(
+                              'Conta',
+                              style: TextStyle(
+                                color: Colors.grey,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            setState(() {
+                              _showMoney = !_showMoney;
+                            });
+                          },
+                            child:
+                                Icon(Icons.remove_red_eye, color: Colors.grey))
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 20, top: 20, bottom: 20),
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Saldo disponível',
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                          ),
+                          _showMoney ? Text.rich(
+                            TextSpan(
+                              text: 'R\$ 1.124.546,00',
+                            ),
+                            textAlign: TextAlign.start,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                            ),
+                          ) : Text('R\$ ************', style: TextStyle(color: Colors.black, fontSize: 30))
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.05,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Icon(
+                      Icons.credit_card,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    Flexible(
+                      child: Text(
+                        'Compra mais recente em Shopping Center Plaza no valor de R\$ 5.200,00 sexta ',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.chevron_right,
+                      color: Colors.grey,
+                      size: 20,
+                    ),
+                  ],
+                ),
+              ),
+              color: Colors.grey[300],
+            ),
+          )
+        ],
+      ),
     );
   }
+  
+  // Manter estado
+  @override
+  bool get wantKeepAlive => true;
 }
--- lib/pages/home/widgets/third_card.dart
+++ lib/pages/home/widgets/third_card.dart
@@ -5,13 +5,52 @@ class ThirdCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Container(),
-        Container(
-          color: Colors.grey,
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(30),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Icon(Icons.wallet_giftcard),
+          Column(
+            children: [
+              Text(
+                'Nubank Rewards',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30),
+              ),
+              SizedBox(height: 15,),
+              Text(
+                'Acumule pontos que nunca expiram e troque por passagens aéreas ou serviços que você realmente usa.',
+                style: TextStyle(color: Colors.grey[600], fontSize: 18),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Container(
+            height: 40,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              border: Border.all(
+                width: 0.9,
+                color: Colors.purple,
+              ),
+            ),
+            child: ElevatedButton(
+                child: Text(
+                  'ATIVE O SEU REWARDS',
+                ),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.purple,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5))),
+                onPressed: () {}),
+          )
+        ],
+      ),
     );
   }
 }
